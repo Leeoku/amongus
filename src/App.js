8ad6f42c8950da9de0eee139605fba0f80d2774b
Leeoku <leeoku@gmail.com>
2021-10-23 00:17:58 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { data } from "./data";
+import React, { useState } from 'react'
+import logo from './amongus.png'
 
 function App() {
+  const [id, setId] = useState(" ") 
+  const submitHandler = (e) => {
+    e.preventDefault(); 
+  }
+  const Message = () => {
+    console.log('id',id);
+    const filteredId = data.filter(user=> user.id===id)
+    const message = filteredId.length > 0 ? filteredId[0].message: ""
+    return(<div>
+      <h1 style={{whiteSpace: "pre-line"}}> {message} </h1>
+    </div>)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={logo} alt="amongus" className="logo"></img>
+        <h1>YOUR ID: {id}</h1>
+      </div>
+      <form onSubmit={submitHandler}>
+        <input type="text" onChange={(e) => setId(e.target.value)}></input>
+      </form>
+      <Message />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,71 @@
+export const data = [
+  {
+    id: "198569",
+    message:
+      "HELLO AGENT WITCH AARON \n You know what they’d call you if you lived in the desert? A sand-witch!",
+  },
+  {
+    id: "382119",
+    message: "HELLO AGENT bAAAAAlana",
+  },
+  {
+    id: "121937",
+    message:
+      "HELLO AGENT LUCKY JAMIE\n What’s a leprechaun’s favourite music? …. \n SHAM-ROCK and roll!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\nWhat do you call a dino who had too much curry?\nMega-sore-ass!",
+  },
+  {
+    id: "325969",
+    message:
+      "HELLO AGENT SEXY NURSE KENNY\n How do they know when nurse Kenny’s having a bad day?\n He won’t stop needling people!",
+  },
+  {
+    id: "171540",
+    message:
+      "HELLO AGENT ERIC THE BASSOON\n They may not like you know… bassoon they will!",
+  },
+  {
+    id: "111645",
+    message:
+      "HELLO AGENT GHOST MICHAEL\n I bet you’re a terrible liar Ghost Michael. Why? Because I can SEE RIGHT THROUGH YOU.",
+  },
+  {
+    id: "720169",
+    message:
+      "HELLO AGENT SAGNIK SPACEMAN\nWe’re throwing a party in space…. Can you help us planet?",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+  {
+    id: "650000",
+    message:
+      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+  },
+];
