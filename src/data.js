91a519a64dce71b6470a2331d28aca3022832da6
Leeoku <leeoku@gmail.com>
2021-10-29 12:53:48 -0400

    chore: add data

--- src/data.js
+++ src/data.js
@@ -2,70 +2,59 @@ export const data = [
   {
     id: "198569",
     message:
-      "HELLO AGENT WITCH AARON \n You know what they’d call you if you lived in the desert? A sand-witch!",
+      "HELLO AGENT WITCH AARON! \n You know what they’d call you if you lived in the desert? A sand-witch!",
   },
   {
     id: "382119",
-    message: "HELLO AGENT bAAAAAlana",
+    message: "HELLO AGENT bAAAAAlana!",
   },
   {
     id: "121937",
     message:
-      "HELLO AGENT LUCKY JAMIE\n What’s a leprechaun’s favourite music? …. \n SHAM-ROCK and roll!",
+      "HELLO AGENT LUCKY JAMIE!\n What’s a leprechaun’s favourite music? …. \n SHAM-ROCK and roll!",
   },
   {
     id: "650000",
     message:
-      "HELLO AGENT DINO JENN\nWhat do you call a dino who had too much curry?\nMega-sore-ass!",
+      "HELLO AGENT DINO JENN!\nWhat do you call a dino who had too much curry?\nMega-sore-ass!",
   },
   {
     id: "325969",
     message:
-      "HELLO AGENT SEXY NURSE KENNY\n How do they know when nurse Kenny’s having a bad day?\n He won’t stop needling people!",
+      "HELLO AGENT SEXY NURSE KENNY!\n How do they know when nurse Kenny’s having a bad day?\n He won’t stop needling people!",
   },
   {
     id: "171540",
     message:
-      "HELLO AGENT ERIC THE BASSOON\n They may not like you know… bassoon they will!",
+      "HELLO AGENT ERIC THE BASSOON! \n They may not like you now… bassoon they will!",
   },
   {
-    id: "111645",
+    id: "105000",
     message:
-      "HELLO AGENT GHOST MICHAEL\n I bet you’re a terrible liar Ghost Michael. Why? Because I can SEE RIGHT THROUGH YOU.",
+      "HELLO AGENT MR. RIGBEAR!\n Why are your children so spoiled? Because you PANDA’D to our every need!",
   },
   {
-    id: "720169",
+    id: "824895",
     message:
-      "HELLO AGENT SAGNIK SPACEMAN\nWe’re throwing a party in space…. Can you help us planet?",
+      "HELLO AGENT MOMBIE RIGBY!\n What kind of currency do zombie’s use CRYPT-o-currency",
   },
   {
-    id: "650000",
+    id: "111318",
     message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+      "HELLO AGENT!\n How do you find a Greek sandwich? With a GYRO SCOPE ;D",
   },
   {
-    id: "650000",
-    message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+    id: "122526",
+    message: "HELLO AGENT!\n ALLL HAIL THE PRINCE OF DARKNESS",
   },
   {
-    id: "650000",
+    id: "145500",
     message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+      "HELLO AGENT RUBBER MICHELLE!\n Why did Mozart hate chickens? …. All they do is say BACH BACK BACH",
   },
   {
-    id: "650000",
+    id: "525977",
     message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
-  },
-  {
-    id: "650000",
-    message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
-  },
-  {
-    id: "650000",
-    message:
-      "HELLO AGENT DINO JENN\n What do you call a dino who had too much curry?\n Mega-sore-ass!",
+      "HELLO AGENT CHEWBACANNE!\n One time Chew forgot to put the parking break on on the car. It was a WOOKIE mistake",
   },
 ];
